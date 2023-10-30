7c4eed261c00af44bfaee7e0a17c329ddb93d500
Tim Holmes <taholmes160@gmail.com>
2023-10-30 19:20:42 -0400

    Add sorting by gender and level in student list
    Updated the student list view to allow sorting by gender and level. This was achieved by modifying the query in the list_students function in students.py to join with the Gender and Level models when the sort parameter is 'gender.gender_name' or 'level.level_name'. Also updated the list_students.html template to include links for sorting by these fields.

--- app/students.py
+++ app/students.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request
-from app.models import Student
+from app.models import Student, Level, Gender
 from sqlalchemy import or_
 
 students_bp = Blueprint('students', __name__)
@@ -12,11 +12,11 @@ def list_students():
     search_query = request.args.get('search', '')
     sort_by = request.args.get('sort', 'student_id')
 
-    students = Student.query.filter(
-        or_(
-            Student.student_fname.ilike(f'%{search_query}%'),
-            Student.student_lname.ilike(f'%{search_query}%')
-        )
-    ).order_by(sort_by).paginate(page=page, per_page=per_page)
+    if sort_by == 'gender.gender_name':
+        students = Student.query.join(Student.gender).order_by(Gender.gender_name).paginate(page=page, per_page=per_page)
+    elif sort_by == 'level.level_name':
+        students = Student.query.join(Student.level).order_by(Level.level_name).paginate(page=page, per_page=per_page)
+    else:
+        students = Student.query.order_by(sort_by).paginate(page=page, per_page=per_page)
 
     return render_template('students/list_students.html', students=students, search_query=search_query, sort_by=sort_by)
