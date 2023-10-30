ddf92ced5c9e69b65fb7025d60d23f36b9e20499
Tim Holmes <taholmes160@gmail.com>
2023-10-30 18:53:11 -0400

    Add search and sort functionality to student list
    This commit introduces a search and sort functionality to the student list page. Users can now search for students by their first or last name. The search is case-insensitive and uses the SQL 'ilike' function for pattern matching.
    
    In addition, users can sort the list of students by either their student ID, first name, or last name. The sort order is determined by the 'sort' query parameter in the URL.
    
    The number of students displayed per page has been reduced from 25 to 20 to accommodate the new search and sort UI elements. The pagination links have been updated to include the current search query and sort order in their URLs.
    
    The student list template has been updated to include a search form and sort links in the table headers.

--- app/students.py
+++ app/students.py
@@ -1,13 +1,22 @@
 from flask import Blueprint, render_template, request
 from app.models import Student
+from sqlalchemy import or_
 
 students_bp = Blueprint('students', __name__)
 
 @students_bp.route('/students', methods=['GET'])
 def list_students():
     page = request.args.get('page', 1, type=int)
-    per_page = 25  # Number of students per page
+    per_page = 20  # Number of students per page
 
-    students = Student.query.paginate(page=page, per_page=per_page)
+    search_query = request.args.get('search', '')
+    sort_by = request.args.get('sort', 'student_id')
 
-    return render_template('students/list_students.html', students=students)
+    students = Student.query.filter(
+        or_(
+            Student.student_fname.ilike(f'%{search_query}%'),
+            Student.student_lname.ilike(f'%{search_query}%')
+        )
+    ).order_by(sort_by).paginate(page=page, per_page=per_page)
+
+    return render_template('students/list_students.html', students=students, search_query=search_query, sort_by=sort_by)
